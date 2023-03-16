99efbc9b12b8dc83b5edb0cab069dd02e75e47c8
Trong-Thang Pham <phamtrongthang123@gmail.com>
2023-03-16 15:29:10 -0500

    baseline4 still working

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,5 @@
 from .baseline import GazeBaseline
 from .baseline2 import GazeBaseline2
 from .baseline3 import GazeBaseline3
+from .baseline4 import GazeBaseline4
+from .baseline5 import GazeBaseline5
