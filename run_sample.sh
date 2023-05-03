8d3f56d9ddd106045b507deaff8cf01e41448bff
Trong-Thang Pham <phamtrongthang123@gmail.com>
2023-05-03 13:29:00 -0500

    test overfit ability

--- run_sample.sh
+++ run_sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate env_torchism/
-CUDA_VISIBLE_DEVICES=0 python train.py --config configs/train/gazebaseline7_subtest1.yaml --gpus 0
+CUDA_VISIBLE_DEVICES=0 python train.py --config configs/train/gazebaseline7_subtest1_overfit.yaml --gpus 0
